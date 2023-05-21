3bfd23ff82eb1932524e1829867a929b661f39dc
Connor <hello@connorhanks.com>
2023-05-21 01:08:52 +0100

    Redesigned login/registration forms, added validation checks

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
-import { useNavigate } from "react-router-dom";
-import Login from "./Login";
+import { useNavigate, Link } from "react-router-dom";
 
 const RegistrationPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [registered, setRegistered] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
   const notyf = new Notyf();
   const navigate = useNavigate();
 
@@ -19,12 +18,21 @@ const RegistrationPage = () => {
     setPassword(event.target.value);
   };
 
-  const handleRegistration = () => {
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleRegistration = (event) => {
+    event.preventDefault();
+
+    // Create an object with the form data
     const formData = {
       email: email,
       password: password,
+      confirmPassword: confirmPassword,
     };
 
+    // Make a POST request to the server
     fetch("/register", {
       method: "POST",
       headers: {
@@ -34,46 +42,125 @@ const RegistrationPage = () => {
     })
       .then((response) => response.json())
       .then((data) => {
+        // Handle the response from the server
         console.log(data);
 
+        // Check if the registration was successful
         if (data.message === "Registration successful") {
+          // Display a success notification
           notyf.success("Registration successful");
 
-          setRegistered(true);
+          // Reset the form
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
 
+          // Redirect to the login page
           navigate("/login");
         } else if (data.message === "Email already taken") {
           notyf.error("Email already taken");
+        } else {
+          notyf.error("Invalid registration");
         }
       })
       .catch((error) => {
+        // Handle any errors
         console.error("Error:", error);
       });
   };
 
-  if (registered) {
-    return <Login />;
-  }
-
   return (
-    <div className="registration-page min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md">
-        <h2 className="text-2xl mb-4">Registration Page</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block mb-2">Email:</label>
-            <input type="email" className="w-full border rounded p-2" value={email} onChange={handleEmailChange} required />
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+          Social-Network
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create an account</h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleRegistration}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="••••••••"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  name="confirm-password"
+                  id="confirm-password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="••••••••"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                  required
+                />
+              </div>
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
+                    I accept the{" "}
+                    <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Login
+                </Link>
+              </p>
+            </form>
           </div>
-          <div className="mb-4">
-            <label className="block mb-2">Password:</label>
-            <input type="password" className="w-full border rounded p-2" value={password} onChange={handlePasswordChange} required />
-          </div>
-          <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleRegistration}>
-            Register
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
