04d6c014b69455d893172df73b3781f05f4828c1
Connor <hello@connorhanks.com>
2023-05-28 18:07:50 +0100

    Updated registration component to include all required fields

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -4,11 +4,39 @@ import "notyf/notyf.min.css";
 import { useNavigate, Link } from "react-router-dom";
 
 const RegistrationPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [avatar, setAvatar] = useState(null);
+  const [bio, setBio] = useState("");
+
   const notyf = new Notyf();
   const navigate = useNavigate();
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleAgeChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -20,6 +48,16 @@ const RegistrationPage = () => {
 
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
+  };
+
+  const handleAvatarChange = (event) => {
+    // Handle avatar file upload here
+    const file = event.target.files[0];
+    setAvatar(file);
+  };
+
+  const handleBioChange = (event) => {
+    setBio(event.target.value);
   };
 
   const handleRegistration = (event) => {
@@ -69,6 +107,71 @@ const RegistrationPage = () => {
       });
   };
 
+  // NEW UPDATED CODE FOR REGISTRATION
+  // const handleRegistration = (event) => {
+  //   event.preventDefault();
+
+  //   // Create an object with the form data
+  //   const formData = {
+  //     firstName: firstName,
+  //     lastName: lastName,
+  //     username: username,
+  //     age: age,
+  //     gender: gender,
+  //     email: email,
+  //     password: password,
+  //     confirmPassword: confirmPassword,
+  //     avatar: avatar,
+  //     bio: bio
+  //   };
+
+  //   // Create a FormData object for sending the data as multipart/form-data
+  //   const formDataToSend = new FormData();
+  //   for (const key in formData) {
+  //     formDataToSend.append(key, formData[key]);
+  //   }
+
+  //   // Make a POST request to the server
+  //   fetch("/register", {
+  //     method: "POST",
+  //     body: formDataToSend
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Handle the response from the server
+  //       console.log(data);
+
+  //       // Check if the registration was successful
+  //       if (data.message === "Registration successful") {
+  //         // Display a success notification
+  //         notyf.success("Registration successful");
+
+  //         // Reset the form
+  //         setFirstName("");
+  //         setLastName("");
+  //         setUsername("");
+  //         setAge("");
+  //         setGender("");
+  //         setEmail("");
+  //         setPassword("");
+  //         setConfirmPassword("");
+  //         setAvatar(null);
+  //         setBio("");
+
+  //         // Redirect to the login page
+  //         navigate("/login");
+  //       } else if (data.message === "Email already taken") {
+  //         notyf.error("Email already taken");
+  //       } else {
+  //         notyf.error("Invalid registration");
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       // Handle any errors
+  //       console.error("Error:", error);
+  //     });
+  // };
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -80,6 +183,76 @@ const RegistrationPage = () => {
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create an account</h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleRegistration}>
+              <div>
+                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Your first name"
+                  value={firstName}
+                  onChange={handleFirstNameChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Your last name"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Choose a username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="age" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Age
+                </label>
+                <input
+                  type="number"
+                  name="age"
+                  id="age"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Your age"
+                  value={age}
+                  onChange={handleAgeChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Gender
+                </label>
+                <input
+                  type="text"
+                  name="gender"
+                  id="gender"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Your gender"
+                  value={gender}
+                  onChange={handleGenderChange}
+                />
+              </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your email
@@ -125,38 +298,58 @@ const RegistrationPage = () => {
                   required
                 />
               </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-                    I accept the{" "}
-                    <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
+              <div>
+                <label htmlFor="avatar" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Avatar
+                </label>
+                <input type="file" name="avatar" id="avatar" accept="image/*" className="hidden" onChange={handleAvatarChange} />
+                <label
+                  htmlFor="avatar"
+                  className="flex items-center justify-center w-full px-6 py-3 mt-2 text-sm font-medium text-white transition duration-200 ease-in bg-primary-600 border border-transparent rounded-lg cursor-pointer hover:bg-primary-700 focus:outline-none focus:border-primary-700 focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                >
+                  Select Avatar
+                </label>
               </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Create an account
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
-                <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                  Login
-                </Link>
-              </p>
+              <div>
+                <label htmlFor="bio" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Bio
+                </label>
+                <textarea
+                  name="bio"
+                  id="bio"
+                  rows="3"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Tell us about yourself"
+                  value={bio}
+                  onChange={handleBioChange}
+                ></textarea>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="flex justify-center w-full px-6 py-3 mt-2 text-sm font-medium text-white transition duration-200 ease-in bg-primary-600 border border-transparent rounded-lg cursor-pointer hover:bg-primary-700 focus:outline-none focus:border-primary-700 focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                >
+                  Register
+                </button>
+              </div>
             </form>
+            <p className="text-xs text-center text-gray-400">
+              By signing up, you agree to our{" "}
+              <a href="#" className="underline">
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a href="#" className="underline">
+                Privacy Policy
+              </a>
+              .
+            </p>
+            <p className="text-sm text-center text-gray-400">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                Log in
+              </Link>
+            </p>
           </div>
         </div>
       </div>
